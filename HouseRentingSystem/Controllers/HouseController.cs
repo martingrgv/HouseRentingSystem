eb239b3bfd0e742f82f02b28c0391579ded7a713
martingrgv <martin.geor04@gmail.com>
2024-07-29 02:25:00 +0300

    Created house page controller, views and models

--- HouseRentingSystem/Controllers/HouseController.cs
+++ HouseRentingSystem/Controllers/HouseController.cs
@@ -1,22 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using HouseRentingSystem.Core.Models.House;
 
 namespace HouseRentingSystem.Controllers
 {
+    [Authorize]
     public class HouseController : Controller
     {
+        [AllowAnonymous]
         public IActionResult All()
         {
-            return View();
+            var model = new AllHousesQueryModel();
+            return View(model);
         }
 
         public IActionResult Mine()
         {
-            return View();
+            var model = new AllHousesQueryModel();
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = new HouseDetailsViewModel();
+            return View(model);
         }
 
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(HouseFormModel model)
+        {
+            return RedirectToAction(nameof(Details), new { id = "1" });
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var model = new HouseFormModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, HouseFormModel house)
+        {
+            return RedirectToAction(nameof(Details), new { id = "1" });
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var model = new HouseFormModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(HouseDetailsViewModel model)
+        {
+            return RedirectToAction(nameof(All));
+        }
+
+        [HttpPost]
+        public IActionResult Rent(int id)
+        {
+            return RedirectToAction(nameof(Mine));
+        }
+
+        [HttpPost]
+        public IActionResult Leave(int id)
+        {
+            return RedirectToAction(nameof(Mine));
         }
     }
 }
