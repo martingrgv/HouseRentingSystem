0f49ca4e2b629467d5c397ad135c05ea2ad67bd7
martingrgv <martin.geor04@gmail.com>
2024-07-29 01:48:05 +0300

    Added views and controllers for houses and become an agent.

---
+++ HouseRentingSystem/Controllers/AgentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    public class AgentsController : Controller
+    {
+        public IActionResult Become()
+        {
+            return View();
+        }
+    }
+}
---
+++ HouseRentingSystem/Controllers/HouseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    public class HouseController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        public IActionResult Mine()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
