9e8791e397fcff4dcf7e8f93140f3bc610938ca2
martingrgv <martin.geor04@gmail.com>
2024-07-29 02:34:55 +0300

    Created agent page controller, view and model

---
+++ HouseRentingSystem/Controllers/AgentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using HouseRentingSystem.Core.Models.Agent;
+
+namespace HouseRentingSystem.Controllers
+{
+    [Authorize]
+    public class AgentController : Controller
+    {
+        public IActionResult Become()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Become(BecomeAgentFormModel model)
+        {
+            return RedirectToAction(nameof(HouseController.All), "House");
+        }
+    }
+}
