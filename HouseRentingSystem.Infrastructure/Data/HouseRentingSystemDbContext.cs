1fce4559a7e5d3f45ff5b172f0314d1b3d686cd9
martingrgv <martin.geor04@gmail.com>
2024-07-29 00:03:02 +0300

    Changed house relations to restrict on delete behavior and applied migrations.

--- HouseRentingSystem.Infrastructure/Data/HouseRentingSystemDbContext.cs
+++ HouseRentingSystem.Infrastructure/Data/HouseRentingSystemDbContext.cs
@@ -12,4 +12,21 @@ public class HouseRentingSystemDbContext : DbContext
     public DbSet<House> Houses { get; set; } = null!;
     public DbSet<Category> Categories { get; set; } = null!;
     public DbSet<Agent> Agents { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<House>()
+            .HasOne(e => e.Category)
+            .WithMany(e => e.Houses)
+            .HasForeignKey(e => e.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<House>()
+            .HasOne(e => e.Agent)
+            .WithMany()
+            .HasForeignKey(e => e.AgentId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
