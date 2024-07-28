c5feadadb9ba44283022ef7744748058839a1aa1
Martin Willson Georgiev <martinwlsngrgv@Azras-MacBook-Pro.local>
2024-07-28 22:09:19 +0300

    Reconstructing all directories

--- HouseRentingSystem.Infrastructure/HouseRentingSystemDbContext.cs
+++ HouseRentingSystem.Infrastructure/HouseRentingSystemDbContext.cs
@@ -4,5 +4,7 @@ namespace HouseRentingSystem.Infrastructure;
 
 public class HouseRentingSystemDbContext : DbContext
 {
+    public HouseRentingSystemDbContext() {}
 
+    public HouseRentingSystemDbContext(DbContextOptions options) : base(options) {}
 }
