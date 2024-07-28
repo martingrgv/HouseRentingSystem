65b83504f1821cf64d1421f5339e43d4513e9669
Martin Willson Georgiev <martinwlsngrgv@Azras-MacBook-Pro.local>
2024-07-28 21:50:38 +0300

    Cleaned project

--- HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
+++ HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -1,6 +1,36 @@
-﻿namespace Microsoft.Extensions.DependencyInjection;
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Services;
+using HouseRentingSystem.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
-public class ServiceCollectionExtension
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
 {
+    public static IServiceCollection AddServiceCollection(this IServiceCollection services)
+    {
+        //services.AddScoped<IHouseService, HouseService>();
 
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<HouseRentingSystemDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationIdentity(this IServiceCollection services)
+    {
+        services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<HouseRentingSystemDbContext>();
+
+        return services;
+    }
 }
--- HouseRentingSystem/obj/Debug/net6.0/HouseRentingSystem.AssemblyInfo.cs
+++ HouseRentingSystem/obj/Debug/net6.0/HouseRentingSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HouseRentingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26448b3cb07e974906b188fdd4255123914dc5ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("HouseRentingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HouseRentingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
