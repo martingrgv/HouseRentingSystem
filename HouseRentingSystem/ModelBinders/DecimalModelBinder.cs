c84bd15ee0c1fb6414009eabd082fb85ddac0318
Martin Willson Georgiev <martinwlsngrgv@Azras-MacBook-Pro.local>
2024-07-29 17:11:37 +0300

    Implemented decimal model binder

---
+++ HouseRentingSystem/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace HouseRentingSystem.ModelBinders;
+
+public class DecimalModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        ValueProviderResult valueResult = bindingContext.ValueProvider
+            .GetValue(bindingContext.ModelName);
+
+        if (valueResult != ValueProviderResult.None &&
+            !string.IsNullOrEmpty(valueResult.FirstValue))
+        {
+            decimal result = default;
+            bool success = false;
+
+            try
+            {
+                string decimalValue = valueResult.FirstValue;
+                decimalValue = decimalValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                decimalValue = decimalValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                result = Convert.ToDecimal(decimalValue, CultureInfo.CurrentCulture);
+                success = true;
+            }
+            catch (FormatException e)
+            {
+                bindingContext.ModelState.AddModelError(bindingContext.ModelName, e, bindingContext.ModelMetadata);
+            }
+
+            if (success)
+            {
+                bindingContext.Result = ModelBindingResult.Success(result);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
