c6eab040628805578a5be4c7a0fc30176e6475cc
martingrgv <martin.geor04@gmail.com>
2024-07-29 12:38:17 +0300

    Added message constants

---
+++ HouseRentingSystem.Core/Constants/MessageConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace HouseRentingSystem.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string RequiredMessage = "The {0} is required!";
+
+        public const string LengthMessage = "The {0} must be between {2} and {1} characters!";
+
+        public const string PhoneErrorMessage = "The {0} must be a valid phone number!";
+    }
+}
