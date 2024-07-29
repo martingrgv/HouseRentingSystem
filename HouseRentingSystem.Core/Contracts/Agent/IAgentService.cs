d771a6bf258ff6fdc45509383d3d320a45041ed7
martingrgv <martin.geor04@gmail.com>
2024-07-29 12:02:00 +0300

    Created agent service exist by id method

--- HouseRentingSystem.Core/Contracts/Agent/IAgentService.cs
+++ HouseRentingSystem.Core/Contracts/Agent/IAgentService.cs
@@ -2,5 +2,6 @@
 {
     public interface IAgentService
     {
+        Task<bool> ExistsById(string userId);
     }
 }
--- HouseRentingSystem.Core/Services/Agent/AgentService.cs
+++ HouseRentingSystem.Core/Services/Agent/AgentService.cs
@@ -1,8 +1,22 @@
 ﻿using HouseRentingSystem.Core.Contracts.Agent;
+using HouseRentingSystem.Infrastructure.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseRentingSystem.Core.Services.Agent
 {
     public class AgentService : IAgentService
     {
+        private readonly IRepository repository;
+
+        public AgentService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<bool> ExistsById(string userId)
+        {
+            return await repository.AllReadOnly<Infrastructure.Data.Models.Agent>()
+                .AnyAsync(a => a.UserId == userId);
+        }
     }
 }
