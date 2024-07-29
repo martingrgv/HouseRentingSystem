d7aeffb9bf807fa6841dfddc16f587dc79be0193
martingrgv <martin.geor04@gmail.com>
2024-07-29 12:37:59 +0300

    Implemented agent form model

--- HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
+++ HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,15 @@
-﻿namespace HouseRentingSystem.Core.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+using static HouseRentingSystem.Infrastructure.Constants.ValidationConstants;
+
+namespace HouseRentingSystem.Core.Models.Agent
 {
     public class BecomeAgentFormModel
     {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(AgentPhoneNumberMaxLength, MinimumLength = AgentPhoneNumberMinLength, ErrorMessage = LengthMessage)]
+        [Display(Name = "Phone number")]
+        [Phone(ErrorMessage = PhoneErrorMessage)]
+        public string PhoneNumber { get; set; } = null!;
     }
 }
